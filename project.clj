b672988029d57f328e64e1206389a6b5b4d6e275
Tom Crayford <tcrayford@googlemail.com>
2014-03-28 16:25:49 +0000

    fix up project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject liza "0.1.0-SNAPSHOT"
   :global-vars {*warn-on-reflection* true}
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/tcrayford/liza"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
